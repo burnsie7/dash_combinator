a9a959deffd0e336dcdfbe5b437fb330eb7d028f
bburns-ddog <benjamin.burns@datadoghq.com>
2018-04-30 22:39:22 -0700

    more stuff

--- dash_combinator.py
+++ dash_combinator.py
@@ -1,16 +1,8 @@
 import json
 import sys
 
+from argparse import ArgumentParser
 from pprint import pprint
-
-source_dash = json.load(open('quick_jenkins.json'))
-new_dash = json.load(open('quick_k8s.json'))
-
-source_widgets = source_dash['widgets']
-new_widgets = new_dash['widgets']
-
-source_tvars = source_dash['template_variables']
-new_tvars = new_dash['template_variables']
 
 def query_yes_no(question, default="yes"):
     """Ask a yes/no question via raw_input() and return their answer.
@@ -52,21 +44,41 @@ def combine_and_export(export_widgets, export_tvars, filename):
     with open(filename, 'w') as outfile:
         json.dump(source_dash, outfile, indent=4, sort_keys=True)
 
-def export_widgets(widgets):
+def export_widgets(widgets, dash_type):
     export_widgets = []
-    for widget in widgets:
-        title = widget.get('title_text', None) or widget.get('query', 'No title or query specified')
-        w_type = widget.get('type', 'No type specified')
-        if w_type == 'note':
-            title = widget.get('html', 'No title or query specified')
-        elif w_type == 'image':
-            title = widget.get('url', 'No title or query specified')
-        print("Parsing widget...")
-        print("Title = {}".format(title))
-        print("Type = {}".format(w_type))
-        clone = query_yes_no("Would you like to clone this widget?")
-        if clone:
-            export_widgets.append(widget)
+    if dash_type == 'screenboard':
+        for widget in widgets:
+            title = widget.get('title_text', None) or widget.get('query', None).get('')
+            w_type = widget.get('type', 'No type specified')
+            if w_type == 'note':
+                title = widget.get('html', 'No title or query specified')
+            elif w_type == 'image':
+                title = widget.get('url', 'No title or query specified')
+                print("Parsing widget...")
+                print("Title = {}".format(title))
+                print("Type = {}".format(w_type))
+                clone = query_yes_no("Would you like to clone this widget?")
+                if clone:
+                    export_widgets.append(widget)
+    elif dash_type == 'timeboard':
+        for widget in widgets:
+            title = widget.get('title', None)
+            if not title:
+                try:
+                    title = widget['definition']['requests'][0]['query']
+                except KeyError:
+                    title = 'No Title'
+            w_type = 'Unknown'
+            try:
+                w_type = widget['definition']['viz']
+            except KeyError:
+                pass
+            print("Parsing widget...")
+            print("Title = {}".format(title))
+            print("Type = {}".format(w_type))
+            clone = query_yes_no("Would you like to clone this widget?")
+            if clone:
+                export_widgets.append(widget)
     return export_widgets
 
 def export_tvars(tvars, widgets):
@@ -143,10 +155,61 @@ def update_x(widgets, x):
         if w['y'] < 2:
             w['y'] = 0
 
-source_max_x = get_max_x(source_widgets)
-update_x(new_widgets, source_max_x)
-cloned_widgets = export_widgets(new_widgets)
-cloned_tvars, cloned_widgets = export_tvars(new_tvars, cloned_widgets)
-print("vars len = {}".format(len(cloned_tvars)))
-print("widgets len = {}".format(len(cloned_widgets)))
-combine_and_export(cloned_widgets, cloned_tvars, 'test_var_export.json')
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Download dashboard as JSON and create new Dash from JSON')
+    parser.add_argument('-s', help='Source file', required=True)
+    parser.add_argument('-n', help='New file', required=True)
+    parser.add_argument('-d', help='Destination File', required=True)
+
+    args = parser.parse_args()
+
+    source_file = args.s
+    new_file = args.n
+    dest_file = args.d
+
+    source_dash = json.load(open(source_file))
+    new_dash = json.load(open(new_file))
+    dash_type = ''
+
+    if source_dash.get('graphs', None):
+        source_dash_type = 'timeboard'
+    elif source_dash.get('widgets', None):
+        source_dash_type = 'screenboard'
+    else:
+        print('Unable to determine board type.  Exiting.')
+        exit()
+
+    if new_dash.get('graphs', None):
+        new_dash_type = 'timeboard'
+    elif new_dash.get('widgets', None):
+        new_dash_type = 'screenboard'
+    else:
+        print('Unable to determine board type.  Exiting.')
+        exit()
+
+    source_max_x = 0
+    if source_dash_type == 'screenboard':
+        source_widgets = source_dash['widgets']
+        source_max_x = get_max_x(source_widgets)
+    elif source_dash_type == 'timeboard':
+        source_widgets = source_dash['graphs']
+
+    if new_dash_type == 'screenboard':
+        new_widgets = new_dash['widgets']
+        update_x(new_widgets, source_max_x)
+    elif new_dash_type == 'timeboard':
+        new_widgets = new_dash['graphs']
+
+    source_tvars = source_dash['template_variables']
+    new_tvars = new_dash['template_variables']
+
+    if source_dash_type == 'screenboard' and new_dash_type == 'screenboard':
+        cloned_widgets = export_widgets(new_widgets, new_dash_type)
+        print("cloned widgets = {}".format(cloned_widgets))
+        cloned_tvars, cloned_widgets = export_tvars(new_tvars, cloned_widgets)
+        print("vars len = {}".format(len(cloned_tvars)))
+        print("widgets len = {}".format(len(cloned_widgets)))
+        combine_and_export(cloned_widgets, cloned_tvars, dest_file)
+    else:
+        print("Only screenboards supported currently")
+        exit()
